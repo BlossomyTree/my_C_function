0e11ef3682bda051f4314192ac001f70f62971df
HeCongXiao <157207450+BlossomyTree@users.noreply.github.com>
2024-10-16 15:04:24 +0800

    Update and rename my_strpy.c to my_strstr.c

---
+++ my_strstr.c
@@ -0,0 +1,26 @@
+char* my_strstr(const char* p1, const char* p2)
+{
+	assert(p1);
+	assert(p2);
+	char* s1, * s2;
+	char* cur = (char*)p1;
+	if (!*p2)
+		return (char*)p1;
+	while (*cur)
+	{
+		s1 = cur;
+		s2 = (char*)p2;
+		while (*s1 && *s2 && !(*s1 - *s2))
+		{
+			s1++;
+			s2++;
+		}
+		if (!*s2)
+			return cur;
+		cur++;
+		/*if (!*s1)
+			return NULL;*///后面那个return NULL和while(*cur)配合相当于实现了这样一个功能
+
+	}
+	return NULL;
+	//拓展：KMP算法--字符串匹配算法（效率更高）
